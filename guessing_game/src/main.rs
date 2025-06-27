5e2b3dab4732721b64a2c84ebcaa6fb1ced67543
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-06-27 09:15:19 +0530

    project paused as i have no idea how its going will do after completing few chapters

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,18 @@
+// to take user inouts
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(low:1,high:101);
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("you guessed : {}", guess);
 }
