f222b7446a9ab56569c943e95271da09ff50708c
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-06-28 22:04:52 +0530

    references

--- ownership/src/reference_borrowing.rs
+++ ownership/src/reference_borrowing.rs
@@ -1,11 +1,13 @@
 fn main () {
     let s1 = String::from("hello");
-    let (s2,len) =  caluclate_length(s1);
+    let len =  caluclate_length(s1);
     println!("length of {}",s2,len);
 }
 
-fn caluclate_length(s:String) -> (String,usize) {
-    let length = s.len(); //len() returns length of stirng
-    (s,length);
+// s takes the reference of string s1 rather than ownership it points to s1 and s1 is pointer in heap stored references are immutable by default 
 
+// passing in refernces as func parameteres is called borrowing 
+fn caluclate_length(s: &String) -> usize {
+    let length = s.len(); //len() returns length of stirng
+    length;
 }
